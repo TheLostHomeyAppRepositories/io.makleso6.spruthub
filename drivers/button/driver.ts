e30e0477ed364e40cc745fe82f58bb0b91cbfcec
makleso6 <makleso6@gmail.com>
2025-03-15 10:45:24 +0300

    fix button multy_button_clicks

--- drivers/button/driver.ts
+++ drivers/button/driver.ts
@@ -7,50 +7,58 @@ class ButtonDriver extends SprutHubDriver {
         await super.onInit();
         this.log('ButtonDriver has been initialized');
 
-        this.homey.flow.getDeviceTriggerCard('multy_button_clicks')
-            .registerArgumentAutocompleteListener('button', async (query, args) => {
-                const device = args.device as SprutHubDevice;
-                const data = device.getData()
-                const services = await (device.driver as SprutHubDriver).getServices(data.aid);
-                const filter = services?.filter(service => service.type === 'StatelessProgrammableSwitch')
-                return filter?.map(service => ({
-                    name: service.name,
-                    aid: service.aId,
-                    sid: service.sId
-                })) || [];;
+        let triggerCard = this.homey.flow.getDeviceTriggerCard('multy_button_clicks')
+        triggerCard.registerArgumentAutocompleteListener('button', async (query, args) => {
+            const device = args.device as SprutHubDevice;
+            const data = device.getData()
+            const services = await (device.driver as SprutHubDriver).getServices(data.aid);
+            const filter = services?.filter(service => service.type === 'StatelessProgrammableSwitch')
+            for (const value of filter || []) {
+                console.log('*** aid', value.aId, typeof value.aId);
+                console.log('*** sid', value.sId, typeof value.sId);
 
-            })
-            .registerArgumentAutocompleteListener('click_type', async (query, args) => {
-                if (!(args.button)) {
-                    return [];
-                }
-                const device = args.device as SprutHubDevice;
-                const data = device.getData()
-                const service = await (device.driver as SprutHubDriver).getService(args.button.aid, args.button.sid);
+            }
+            return filter?.map(service => ({
+                name: service.name,
+                aid: Number(service.aId),
+                sid: Number(service.sId)
+            })) || [];;
 
-                return service.characteristics?.flatMap(characteristic => {
-                    if (getCharacteristicControl(characteristic).type === 'ProgrammableSwitchEvent') {
-                        const val = getCharacteristicControl(characteristic).validValues
-                            ?.filter(value => value.checked === true)
-                            ?.map(filteredValue => ({
-                                name: filteredValue.name,
-                                value: filteredValue,
-                                aId: characteristic.aId,
-                                sId: characteristic.sId,
-                                cId: characteristic.cId
-                            })) || [];
-                        console.log(val);
-                        return val
-                    }
-                    return [];
-                }) || [];
-            })
-            .registerRunListener(async (args, state) => {
-                if (state.aId === args.click_type.aId && state.sId === args.click_type.sId && state.cId === args.click_type.cId) {
-                    return Object.values(state.control.value)[0] === Object.values(args.click_type.value.value)[0];
+        })
+        triggerCard.registerArgumentAutocompleteListener('click_type', async (query, args) => {
+            if (!(args.button)) {
+                return [];
+            }
+            const device = args.device as SprutHubDevice;
+            const data = device.getData()
+            console.log('*** aid', args.button.aid, typeof args.button.aid);
+            console.log('*** sid', args.button.sid, typeof args.button.sid);
+            const service = await (device.driver as SprutHubDriver).getService(Number(args.button.aid), Number(args.button.sid));
+
+            return service.characteristics?.flatMap(characteristic => {
+                if (getCharacteristicControl(characteristic).type === 'ProgrammableSwitchEvent') {
+                    const val = getCharacteristicControl(characteristic).validValues
+                        ?.filter(value => value.checked === true)
+                        ?.map(filteredValue => ({
+                            name: filteredValue.name,
+                            value: filteredValue,
+                            aId: characteristic.aId,
+                            sId: characteristic.sId,
+                            cId: characteristic.cId
+                        })) || [];
+                    return val
                 }
-                return false;
-            });
+                return [];
+            }) || [];
+        })
+        triggerCard.registerRunListener(async (args, state) => {
+            if (state.aId === args.click_type.aId && state.sId === args.click_type.sId && state.cId === args.click_type.cId) {
+                let value = Object.values(state.control.value)[0] === Object.values(args.click_type.value.value)[0];
+                console.log('*** value', value);
+                return value
+            }
+            return false;
+        });
     }
 
     async onPairListDevices() {
@@ -79,3 +87,6 @@ class ButtonDriver extends SprutHubDriver {
 }
 
 module.exports = ButtonDriver;
+
+
+
